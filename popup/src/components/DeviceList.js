d5cbca27b7bc19302a0a038e4d9a706a4915a678
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-03-14 13:16:47 +0100

    add ability to remove a device from the current token

--- popup/src/components/DeviceList.js
+++ popup/src/components/DeviceList.js
@@ -1,9 +1,27 @@
+/*global chrome*/
+import axios from "axios";
+
 function DeviceList(props) {
+    const handleDelete = async (e, device) => {
+        if (!(e.shiftKey && e.altKey)) return
+
+        const token = (await chrome.storage.local.get(['auth_token'])).auth_token
+
+        await axios({
+            url: `http://localhost:3000/${token}`,
+            method: 'DELETE',
+            data: {
+                name: device.name
+            }
+        })
+    }
+
     return (
         <div className="devices">
             {props.devices.devices.map((device, i) => {
                 return (
                     <div
+                        onContextMenu={(e) => handleDelete(e, device)}
                         key={device.name}
                         onClick={() => props.setView(i)}
                         className={`${i === props.view ? 'active-device' : ''}`}>
