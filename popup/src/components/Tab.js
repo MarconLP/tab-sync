f5b59932025ab367e8610ffb51221cbffdf72c2d
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-03-15 13:33:41 +0100

    show color of tabgroup before tab

--- popup/src/components/Tab.js
+++ popup/src/components/Tab.js
@@ -1,12 +1,26 @@
+/* global chrome */
 import {ReactComponent as CloseIcon} from "../assets/xmark.svg";
 import * as React from "react";
 
 function Tab(props) {
     const {title, url, groupId, favIconUrl} = props.tab
+
+    const colorMap = {
+        grey: '#606468',
+        blue: '#1b72e8',
+        red: '#db3025',
+        yellow: '#faab00',
+        green: '#168138',
+        pink: '#cf1783',
+        purple: '#a142f3',
+        cyan: '#037b83',
+        orange: '#f9903e'
+    }
+
     return (
-        <div key={props.tab.id}>
+        <div key={props.tab.id} className="tab">
             <div>
-                {/*<span>group</span>*/}
+                <div className={`${groupId > 0 ? 'tab-group' : ''}`}> </div>
                 <img
                     src={favIconUrl ? favIconUrl : 'https://www.google.com/chrome/static/images/favicons/favicon-32x32.png'}
                     className="favicon"
