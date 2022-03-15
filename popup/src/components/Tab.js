4bd5ce1a6a103619e1dfee6110a581bc23971cfa
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-03-15 13:36:35 +0100

    open url in a new tab when clicking a tab

--- popup/src/components/Tab.js
+++ popup/src/components/Tab.js
@@ -1,10 +1,18 @@
+/*global chrome*/
 import {ReactComponent as CloseIcon} from "../assets/xmark.svg";
 import * as React from "react";
 
 function Tab(props) {
     const {title, url, groupId, favIconUrl} = props.tab
+
+    const handleClick = () => {
+        chrome.tabs.create({
+            url: url
+        })
+    }
+
     return (
-        <div key={props.tab.id}>
+        <div key={props.tab.id} onClick={handleClick}>
             <div>
                 {/*<span>group</span>*/}
                 <img
